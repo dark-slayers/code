09cdac127e28636f6007b6860f1984a43ce979d4
dark-slayers <23534878@qq.com>
2017-08-18 16:58:50 +0800

    add a optional class

---
+++ src/main/java/person/liuxx/learn/code/java8/optional/OptionalUse.java
@@ -0,0 +1,24 @@
+package person.liuxx.learn.code.java8.optional;
+
+import java.util.Optional;
+
+/** 
+* @author  刘湘湘 
+* @version 1.0.0<br>创建时间：2017年8月16日 下午4:04:24
+* @since 1.0.0 
+*/
+public class OptionalUse
+{
+    /** 
+    * @author  刘湘湘 
+    * @version 1.0.0<br>创建时间：2017年8月16日 下午4:04:24
+    * @since 1.0.0 
+    * @param args
+    */
+    public static void main(String[] args)
+    {
+        Optional<String> o=Optional.of("A").map(s->null);
+        System.out.println(o);
+        System.out.println(o.get());
+    }
+}
