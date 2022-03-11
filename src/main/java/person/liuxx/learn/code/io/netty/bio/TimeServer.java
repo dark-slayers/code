29eda033704bb9bc99f8262ce51cb23707f4de69
刘湘湘 <23534878@qq.com>
2022-03-11 15:41:30 +0800

    bak

---
+++ src/main/java/person/liuxx/learn/code/io/netty/bio/TimeServer.java
@@ -0,0 +1,60 @@
+package person.liuxx.learn.code.io.netty.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+
+/**
+ * @author
+ * 
+ * @version 1.0.0<br>
+ *          创建时间：2022年3月9日 下午5:33:55
+ * 
+ * @since 1.0.0
+ */
+public class TimeServer {
+    /**
+     * @author
+     * 
+     * @version 1.0.0<br>
+     *          创建时间：2022年3月9日 下午5:33:56
+     * 
+     * @since 1.0.0
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        int port = 8080;
+        if (Objects.nonNull(args) && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port : " + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (Objects.nonNull(server)) {
+                System.out.println("The time server close !");
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                server = null;
+            }
+        }
+    }
+}
---
+++ src/main/java/person/liuxx/learn/code/io/netty/bio/TimeServerHandler.java
@@ -0,0 +1,21 @@
+package person.liuxx.learn.code.io.netty.bio;
+
+import java.net.Socket;
+
+/**
+ * @author
+ * 
+ * @version 1.0.0<br>
+ *          创建时间：2022年3月9日 下午5:53:11
+ * 
+ * @since 1.0.0
+ */
+public class TimeServerHandler implements Runnable {
+    public TimeServerHandler(Socket socket) {
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
+}
